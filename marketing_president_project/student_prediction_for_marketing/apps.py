030ed507a483b9d0aa026692cf316ac0405080df
miko <165224330+Meozee@users.noreply.github.com>
2025-07-07 16:02:01 +0700

    structure awal

---
+++ marketing_president_project/student_prediction_for_marketing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudentPredictionForMarketingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'student_prediction_for_marketing'
